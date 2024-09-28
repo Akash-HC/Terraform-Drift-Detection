88e5137222bfc4100ee295fa0952d20a4c8a5f9b
Akash HC <167284366+Akash-HC@users.noreply.github.com>
2024-09-28 11:38:03 +0530

    Initial commit - main configuration file uploaded

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "Project" {
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [ "sg-0315170f9b2efb5a0" ]
+    ami = "ami-0e86e20dae9224db8"
+    subnet_id = "subnet-0a7b5d28324f6a9e1"
+    key_name = "New_key2"
+    tags = {
+        Name = "practice"
+    }
+}
